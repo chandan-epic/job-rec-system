a57103c25e608662d69e572936fdeb222effcd74
sai chandan <124019705+chandan-epic@users.noreply.github.com>
2024-10-11 11:14:22 +0530

    dockerized the application

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ connectDB();
 // route
 app.use('/api/jobs', jobRoutes); // must hit this route with json data.
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
